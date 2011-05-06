51e3c5e970d86d9e888de84f7dce0981dcd1c23d
Chris Marrin <chris@marrin.com>
2011-05-06 09:53:50 -0700

    changed projects to look for build tools at /usr/local/avrtools, where installer puts them

--- projects/etherclock/MyApplication.cpp
+++ projects/etherclock/MyApplication.cpp
@@ -38,7 +38,7 @@ Application::processEvent(EventType type)
     {
         case EV_IDLE:
             //myShiftReg.setOutputEnable(false);
-            myShiftReg.setChar('9');
+            myShiftReg.setChar('7');
             myShiftReg.setOutputEnable(true);
             break;
         default:
