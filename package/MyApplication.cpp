f78ab4427f2525cf7884d85d7b6dded437421023
Chris Marrin <chris@marrin.com>
2014-03-02 18:33:46 -0800

    Added 3 ways to do light blinking.
    Can now do TimerEvent, decicated RTC and Wait loop timing

--- package/MyApplication.cpp
+++ package/MyApplication.cpp
@@ -8,7 +8,7 @@
 #include "System.h"
 #include "EventListener.h"
 #include "RTC.h"
-#include "Timer1.h"
+#include "Timer0.h"
 #include "TimerEventMgr.h"
 
 #include "BlinkErrorReporter.h"
@@ -17,7 +17,21 @@
 //  Sample Blinking Light Application
 //
 // LED is on port PB5, Arduino pin 13
-// 
+//
+// This sample uses 4 different ways of timing 1 second:
+//
+//  Wait loop:      AVR spins for 1 second between changes in LED
+//  Timer event:    1ms TimerEventMgr is created and a TimerEvent counts 1000 cycles and then fires
+//  Dedicated RTC:  Dedicated RTC is created which gives event every second
+//  Shared RTC:     A RTC which shares the 1ms TimerEventMgr is created which gives event event
+//                  every second (assuming TimerEventMgr is precisely 1ms)
+
+// FIXME: Implement Shared RTC
+
+#define WaitLoop
+//#define TimerEvent
+//#define DedicatedRTC
+//#define SharedRTC
 
 using namespace m8r;
 
@@ -34,19 +48,36 @@ public:
     virtual void handleEvent(EventType type, EventParam);
     
     BlinkErrorReporter<Port<LEDPort>, LEDBit, false> m_errorReporter;
-//    TimerEventMgr<Timer1> m_timerEventMgr;
-    RTC<Timer1> m_clock;
+#if defined(WaitLoop)
+#elif defined(TimerEvent)
+    TimerEventMgr<Timer0, TimerClockDIV64> m_timerEventMgr;
+    RepeatingTimerEvent _timerEvent;
+#elif defined(DedicatedRTC)
+    RTC<Timer0> m_clock;
+#elif defined(SharedRTC)
+#endif
     OutputBit<LEDPort, LEDBit> m_LEDPort;
 };
 
 MyApp g_app;
 
 MyApp::MyApp()
-//    : m_timerEventMgr(TimerClockDIV1024, F_CPU / 1024) // ~1s timer
-    : m_clock(TimerClockDIV1, 15999, 1000) // 1s timer
+#if defined(WaitLoop)
+#elif defined(TimerEvent)
+    : _timerEvent(1000)
+#elif defined(DedicatedRTC)
+    : m_clock(TimerClockDIV64, 249, 1000) // 1s timer
+#elif defined(SharedRTC)
+#endif
 
 {
     sei();
+#if defined(WaitLoop)
+#elif defined(TimerEvent)
+    System::startEventTimer(&_timerEvent);
+#elif defined(DedicatedRTC)
+#elif defined(SharedRTC)
+#endif
 }
 
 void
@@ -54,14 +85,21 @@ MyApp::handleEvent(EventType type, EventParam param)
 {
     switch(type)
     {
+#if defined(WaitLoop)
         case EV_IDLE:
+            System::msDelay<1000>();
+            m_LEDPort = !m_LEDPort;
         break;
+#elif defined(TimerEvent)
         case EV_EVENT_TIMER:
             m_LEDPort = !m_LEDPort;
             break;
+#elif defined(DedicatedRTC)
         case EV_RTC_SECONDS:
             m_LEDPort = !m_LEDPort;
             break;
+#elif defined(SharedRTC)
+#endif
         default:
             break;
     }
